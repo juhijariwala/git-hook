01419cd531a5fd6a4176259a7057f69b6b5f95d2
Juhi Jariwala <juhijari@thoughtworks.com>
2017-01-22 12:20:01 +0530

    test5

--- attributes/__version.rb
+++ attributes/__version.rb
@@ -1,2 +1,2 @@
-default['git-hook']['rpm_version']='1.0.0-1'
-default['git-hook']['commit_sha']='826c390c11f9bb44e54e5576620088b92de39cb6'
+default['git-hook']['rpm_version']='1.0.0-2'
+default['git-hook']['commit_sha']='5555badc062426436530a4397669cf6e0bd5205e'
