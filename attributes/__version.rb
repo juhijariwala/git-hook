5555badc062426436530a4397669cf6e0bd5205e
Juhi Jariwala <juhijari@thoughtworks.com>
2017-01-22 12:11:25 +0530

    test4

---
+++ attributes/__version.rb
@@ -0,0 +1,2 @@
+default['git-hook']['rpm_version']='1.0.0-1'
+default['git-hook']['commit_sha']='826c390c11f9bb44e54e5576620088b92de39cb6'
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'git-hook'
+maintainer       'periscope-solutions'
+maintainer_email 'dev@periscope-solutions.com'
+license          'All rights reserved'
+description      'Installs/Configures infra roles'
+long_description 'Installs/Configures infra roles'
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION'))
